2896452586a1db231d4f7b884d1c2caf011ba837
nastish.n <nastish.novikova@yandex.ru>
2021-02-19 21:25:11 +0300

    corrected block profile

--- skript.js
+++ skript.js
@@ -1,1 +1,13 @@
-// alert('Привет, Java');
+// let container = document.querySelector('.container');
+// let editContainer = container.querySelector('.edit');
+// let editButton = container.querySelector('.profile__edit');
+// let popapVisibility = container.querySelector('.popap-overlay__open');
+
+// function showPopap() {
+//     popapVisibility.classList.remove('popap-overlay__open');
+    
+// }
+
+// editButton.addEventListener('click', showPopap);
+
+
