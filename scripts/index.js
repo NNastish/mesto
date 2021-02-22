5d83a04e896feffe259a1a5afc91dc31545533be
nastish.n <nastish.novikova@yandex.ru>
2021-02-23 02:56:11 +0300

    first revew

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,34 @@
 let profileEditButton = document.querySelector("#edit"); //выбираем кнопку редактирования профиля
 let popup = document.querySelector(".popup"); //выбираем блок с попап
 let popupCloseButton = document.querySelector(".popup__close"); //выбираем кнопку закрытия попап
-
-function profileEdit() {
-    popup.classList.add("popup_open"); // функция открыть попап
-}
-
-profileEditButton.addEventListener("click", profileEdit); //действие "клик по кнопке"
-
-function popupClose() {
-    popup.classList.remove("popup_open"); //функция закрыть попап
-}
-popupCloseButton.addEventListener("click", popupClose); // действие "клип по кнопке"
-
 let profileTitle = document.querySelector(".profile__title"); //выбираем заголовок, который нужно поменять
 let profileText = document.querySelector(".profile__text"); //выбираем параграф, который нужно поменять
 let nameInput = document.querySelector("#name"); //выбираем  строку ввода имени, которая будет менять заголовок
 let aboutInput = document.querySelector("#about"); //выбираем  строку ввода описания, которая будет менять параграф
 let formButton = document.querySelector(".form__button"); //выбираем  кнопку, которая будет сохранять изменения
 
+function copyValue() {
+    nameInput.value = profileTitle.textContent; //копирование заголовка
+    aboutInput.value = profileText.textContent; //копирование параграфа
+}
+
+function openPopup() {
+    copyValue();
+    popup.classList.add("popup_opened"); // функция открыть попап
+}
+
+function closePopup() {
+    popup.classList.remove("popup_opened"); //функция закрыть попап
+}
+
 function formSubmitHandler(evt) {
     // функция сохранить новые данные
-    evt.preventDefault(); //отмена стандартного события, чтобы страница не перегружалась? (как на вебинаре)
+    evt.preventDefault(); //отмена стандартного реагирования браузера на событие *поправка от ревьюера :)
     profileTitle.textContent = nameInput.value; // меняем заголовок
     profileText.textContent = aboutInput.value; // меняет параграф
-    popup.classList.remove("popup_open"); // закрываем попап
+    closePopup(); // закрываем попап
 }
-formButton.addEventListener("click", formSubmitHandler); // действие "клик по кнопке"
+
+profileEditButton.addEventListener("click", openPopup); //действие "клик по кнопке"
+popupCloseButton.addEventListener("click", closePopup); // действие "клип по кнопке"
+formButton.addEventListener("click", formSubmitHandler); // действие "клик по кнопке" почему-то не работает с submit
