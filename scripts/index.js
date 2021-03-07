918be8be2207758d15744c114b147670e96260be
nastish.n <nastish.novikova@yandex.ru>
2021-03-07 23:41:05 +0300

    submit for button

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,17 @@
 const popup = document.querySelector(".popup"); //выбираем блок с попап
 const profileEditButton = document.querySelector("#edit"); //выбираем кнопку редактирования профиля
-const popupEdit = document.querySelector(".popup_type_edit"); //выбираем второй попап для редактирования по модификатору
+const popupEditProfile = document.querySelector(".popup_type_edit"); //выбираем второй попап для редактирования по модификатору
 const profileAddButton = document.querySelector("#add"); //выбираем кнопку добавления фотографии
-const popupAdd = document.querySelector(".popup_type_add"); //выбираем второй попап для добавления фотографий по модификатору
+const popupAddCard = document.querySelector(".popup_type_add"); //выбираем второй попап для добавления фотографий по модификатору
 const popupCloseButton = document.querySelectorAll(".popup__close"); //выбираем кнопку закрытия попап
 const profileTitle = document.querySelector(".profile__title"); //выбираем заголовок, который нужно поменять
 const profileText = document.querySelector(".profile__text"); //выбираем параграф, который нужно поменять
 const nameInput = document.querySelector("#name"); //выбираем  строку ввода имени, которая будет менять заголовок
 const aboutInput = document.querySelector("#about"); //выбираем  строку ввода описания, которая будет менять параграф
-const formButton = document.querySelector(".form"); //выбираем  кнопку, которая будет сохранять изменения
+// const formButton = document.querySelector(".form"); //выбираем  кнопку, которая будет сохранять изменения
+const saveNewProfileInfo = document.querySelector('#save-button');
+const createNewCard = document.querySelector('#create-button');
+
 
 const cardTemplate = document.querySelector("#template"); //выбираем темплэйт
 const cardAdd = document.querySelector(".card"); //выбираем заготовку для карточки в теплэйт
@@ -58,43 +61,54 @@ const initialCards = [
           cardsContainer.insertAdjacentHTML("afterbegin", blockCard);
       });
   };
-  addInitialCards(); //вызов функции с добавлением карточек
-
-
-function copyValue() {
-    nameInput.value = profileTitle.textContent; //копирование заголовка
-    aboutInput.value = profileText.textContent; //копирование параграфа
-}
-
-function openPopup(popupElement) {
-    copyValue();
-    popupElement.classList.add("popup_opened"); // функция открыть попап
-    console.log(popupElement.classList);
-}
-
-function closePopup() {
-    popupEdit.classList.remove("popup_opened"); //функция закрыть попап редактирования
-    popupAdd.classList.remove("popup_opened"); //функция закрыть попап добавления фото
-}
-
-function formSubmitHandler(evt) {
-    // функция сохранить новые данные
-    evt.preventDefault(); //отмена стандартного реагирования браузера на событие *поправка от ревьюера
-    profileTitle.textContent = nameInput.value; // меняем заголовок
-    profileText.textContent = aboutInput.value; // меняет параграф
-    closePopup(); // закрываем попап
-}
-
+  
+  
+  function copyValue() {
+      nameInput.value = profileTitle.textContent; //копирование заголовка
+      aboutInput.value = profileText.textContent; //копирование параграфа
+    }
+    
+    function openPopup(popupElement) {
+        copyValue();
+        popupElement.classList.add("popup_opened"); // функция открыть попап
+    }
+    
+    function closePopup() {
+        popupEditProfile.classList.remove("popup_opened"); //функция закрыть попап редактирования
+        popupAddCard.classList.remove("popup_opened"); //функция закрыть попап добавления фото
+    }
+    
+    function submitProfileForm(evt) {
+        // функция сохранить новые данные
+        evt.preventDefault(); //отмена стандартного реагирования браузера на событие *поправка от ревьюера
+        profileTitle.textContent = nameInput.value; // меняем заголовок
+        profileText.textContent = aboutInput.value; // меняет параграф
+        alert("hello");
+        closePopup(); // закрываем попап
+    }
+    
+    
+    function submitCardForm(evt) {
+        
+        evt.preventDefault();
+        alert("sssssss");
+        closePopup();
+    }
+    
+//________________Вызов функций
+addInitialCards(); //вызов функции с добавлением карточек
 profileEditButton.addEventListener("click", function() {
-    openPopup(popupEdit);
+    openPopup(popupEditProfile);
 }); //действие "клик по кнопке редактирования"
+
+profileAddButton.addEventListener("click", function() {
+    openPopup(popupAddCard);
+}); //действие "клик по кнопке добавить фото"
 
 popupCloseButton.forEach((button) => button.addEventListener("click", function() {
     closePopup();
 })); //действие "клик по кнопке, которое закрывает попапы (редактирования и добавления фото)"
 
-formButton.addEventListener("submit", formSubmitHandler); // действие "отправить форму" редактирования
+saveNewProfileInfo.addEventListener("submit", submitProfileForm);
+createNewCard.addEventListener("submit", submitCardForm);
 
-profileAddButton.addEventListener("click", function() {
-    openPopup(popupAdd);
-}); //действие "клик по кнопке добавить фото"
