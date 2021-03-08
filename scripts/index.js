6ba41bcb07b6a66bb19e997ddd15fe2a0a34c79b
nastish.n <nastish.novikova@yandex.ru>
2021-03-09 00:52:36 +0300

    add new card

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,33 @@
+//-------------------------ПОПАПЫ----------------------------
 const popup = document.querySelector(".popup"); //выбираем блок с попап
-const profileEditButton = document.querySelector("#edit"); //выбираем кнопку редактирования профиля
 const popupEditProfile = document.querySelector(".popup_type_edit"); //выбираем второй попап для редактирования по модификатору
-const profileAddButton = document.querySelector("#add"); //выбираем кнопку добавления фотографии
 const popupAddCard = document.querySelector(".popup_type_add"); //выбираем второй попап для добавления фотографий по модификатору
+
+//-------------------------КНОПКИ-----------------------------
+
 const popupCloseButton = document.querySelectorAll(".popup__close"); //выбираем кнопку закрытия попап
+const profileEditButton = document.querySelector("#edit"); //выбираем кнопку редактирования профиля
+const profileAddButton = document.querySelector("#add"); //выбираем кнопку добавления фотографии
+
+//-------------------------КНОПКИ ДЛЯ ОТПРАВКИ ФОРМ-------------
+
+const buttonSaveProfileInfo = document.querySelector('#save-button'); //выбираем кнопку, которая отправляет форму редактирования инфо-профиля
+const buttonCreateCard = document.querySelector('#create-button'); //выбираем кнопку, которая отправляет форму с данными для добавления карточки
+
+//-------------------------ИНПУТЫ И ДАННЫЕ ДЛЯ ИЗМЕНЕНИЙ-------
+
 const profileTitle = document.querySelector(".profile__title"); //выбираем заголовок, который нужно поменять
 const profileText = document.querySelector(".profile__text"); //выбираем параграф, который нужно поменять
 const nameInput = document.querySelector("#name"); //выбираем  строку ввода имени, которая будет менять заголовок
 const aboutInput = document.querySelector("#about"); //выбираем  строку ввода описания, которая будет менять параграф
-// const formButton = document.querySelector(".form"); //выбираем  кнопку, которая будет сохранять изменения
-const saveNewProfileInfo = document.querySelector('#save-button');
-const createNewCard = document.querySelector('#create-button');
 
+
+//-------------------------ТЕМПЛЭЙТ-----------------------------
 
 const cardTemplate = document.querySelector("#template"); //выбираем темплэйт
-const cardAdd = document.querySelector(".card"); //выбираем заготовку для карточки в теплэйт
 const cardsContainer = document.querySelector(".cards"); //выбираем дивчик с карточками
-const titleInput = document.querySelector("#input-title"); //выбираем строку ввода названия
-const linkInput = document.querySelector("#input-link"); //выбираем строку ввода ссылки
+
+//-------------------------МАССИВ-----------------------------
 
 const initialCards = [ 
     {
@@ -45,21 +55,46 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ]; 
+
+  //--------------------ФУНКЦИИ------------------------
+
+
+
+//   function createNewCard(item) {
+//       const newItem = cardTemplate.querySelector(".card").cloneNode(true);
+//       const cardPhoto = newItem.querySelector(".card__photo");
+//       const cardTitle = newItem.querySelector(".card__title");
+
+//       cardTitle.textContent = item.name;
+//       cardPhoto.src = item.link;
+
+//       return newItem;
+
+//   }
+//     function addInitialCards() { //функция с добавлением карточек в секцию
+//         const result = initialCards.map(createNewCard);
+//         cardsContainer.prepend(...result);
+//   };
+
+
+  function addNewCard (item) {
+    return  `
+      <article class="card">
+          <img src="${item.link}" alt="${item.name}" class="card__photo" />
+          <div class="card__lists">
+              <h2 class="card__title">${item.name}</h2>
+              <button type="button" class="card__like"></button>
+           </div>
+      </article>
+      `;
+  };
   
-  function addInitialCards() { //функция с добавлением карточек в див 
+  function addInitialCards() { //функция с добавлением карточек в секцию
 
     initialCards.forEach(function(item) {
-        let blockCard =  `
-          <article class="card">
-              <img src="${item.link}" alt="${item.name}" class="card__photo" />
-              <div class="card__lists">
-                  <h2 class="card__title">${item.name}</h2>
-                  <button type="button" class="card__like"></button>
-               </div>
-          </article>
-          `;
-          cardsContainer.insertAdjacentHTML("afterbegin", blockCard);
-      });
+        let str = addNewCard(item);
+        cardsContainer.insertAdjacentHTML("afterbegin", str); 
+    });
   };
   
   
@@ -83,32 +118,67 @@ const initialCards = [
         evt.preventDefault(); //отмена стандартного реагирования браузера на событие *поправка от ревьюера
         profileTitle.textContent = nameInput.value; // меняем заголовок
         profileText.textContent = aboutInput.value; // меняет параграф
-        alert("hello");
         closePopup(); // закрываем попап
     }
     
     
     function submitCardForm(evt) {
-        
         evt.preventDefault();
-        alert("sssssss");
+
+
+        const titleInput = document.querySelector("#input-title"); //выбираем строку ввода названия
+        const titleValue = titleInput.value;
+
+        const linkInput = document.querySelector("#input-link"); //выбираем строку ввода ссылки
+        const linkValue = linkInput.value;
+        // const titleInput = newItem.querySelector("#input-title"); //выбираем строку ввода названия
+        // const titleValue = titleInput.value;
+
+        // const linkInput = newItem.querySelector("#input-link"); //выбираем строку ввода ссылки
+        // const linkValue = linkInput.value;
+
+        // const newCardForm = createDomNode({ title: titleValue, link: linkValue});
+        // cardsContainer.prepend(newCardForm);
+        // cardsContainer.insertAdjacentHTML('afterbegin', newCardForm);
+        let result = addNewCard({name: titleValue, link: linkValue});
+        cardsContainer.insertAdjacentHTML('afterbegin', result);
+ 
+        closePopup();
+    } 
+
+    // Nastya's attempt to create card by using template
+    function submitViaTemplate(evt) {
+        evt.preventDefault();
+
+        let newItem = createNewCard(evt.value);
+        const titleInput = newItem.querySelector("#input-title"); //выбираем строку ввода названия
+        const titleValue = titleInput.value;
+
+        const linkInput = newItem.querySelector("#input-link"); //выбираем строку ввода ссылки
+        const linkValue = linkInput.value;
+
         closePopup();
     }
     
-//________________Вызов функций
+//-------------------ВЫЗОВ ФУНКЦИИ-------------------------------
+
 addInitialCards(); //вызов функции с добавлением карточек
+
+//-------------------СЛУШАЛКИ СОБЫТИЙ-----------------------------
+
 profileEditButton.addEventListener("click", function() {
     openPopup(popupEditProfile);
-}); //действие "клик по кнопке редактирования"
+}); //действие "клик по кнопке редактирования профиля"
 
 profileAddButton.addEventListener("click", function() {
     openPopup(popupAddCard);
-}); //действие "клик по кнопке добавить фото"
+}); //действие "клик по кнопке добавить фото [+] "
 
 popupCloseButton.forEach((button) => button.addEventListener("click", function() {
     closePopup();
 })); //действие "клик по кнопке, которое закрывает попапы (редактирования и добавления фото)"
 
-saveNewProfileInfo.addEventListener("submit", submitProfileForm);
-createNewCard.addEventListener("submit", submitCardForm);
+buttonSaveProfileInfo.addEventListener("submit", submitProfileForm); //действие "клик отправить форму на смену данных в профиле редактирования"
+buttonCreateCard.addEventListener("submit", submitCardForm); //действие "клик по кнопке добавить фото и название  в карточку "
+
 
